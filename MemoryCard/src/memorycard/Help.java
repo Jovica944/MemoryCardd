9be84f7e540763dca234ca56b654a8630a475996
Jovica944 <jocoo8882@gmail.com>
2017-07-01 03:56:16 +0200

    igrica  memory  card gotova

---
+++ MemoryCard/src/memorycard/Help.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memorycard;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Joco
+ */
+public class Help extends VBox {
+    private Button backBtn;
+    private TextArea content;
+    
+    String text = "Klasična igra za pamćenje. "
+            + "Sastoji se od određenog broja parova karata koji se tokom igre pokušavaju pronaći. "
+            + "Igrač naizmjenično otvara po dvije karte pokušavajući spojiti parove. "
+            + "Pobjenika je onaj koji pronađe sve parove sa najkraćim putem.";
+    
+    
+    public Help(){
+        super(10);
+        backBtn = new Button("Nazad");
+        content = new TextArea(text);
+        content.setEditable(false);
+        super.getChildren().addAll(backBtn, content);
+        super.setPadding(new Insets(10, 10, 10, 10));
+    }
+
+    public Button getBack() {
+        return backBtn;
+    }
+
+    public TextArea getContent() {
+        return content;
+    }
+    
+    
+}
